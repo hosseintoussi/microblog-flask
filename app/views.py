cbf969fe50ab13cf673c80872a3cc5da6ec07f3f
hosseintoussi <hossein@mindvalley.com>
2016-10-12 00:46:20 +0800

    last seen on user, and about_me field

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
+from datetime import datetime
 from .forms import LoginForm
 from .models import User
 
@@ -45,6 +46,10 @@ def login():
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @oid.after_login
 def after_login(resp):
